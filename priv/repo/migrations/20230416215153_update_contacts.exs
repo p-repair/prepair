093e7e1785449e372a7e20c67f01a40848e36e3a
Jean-Philippe Cugnet <jean-philippe@cugnet.eu>
2023-04-16 23:58:18 +0200

    fix(db:contacts): properly store mailerlite_id as a bigint

---
+++ priv/repo/migrations/20230416215153_update_contacts.exs
@@ -0,0 +1,9 @@
+defmodule PrepairLandingPage.Repo.Migrations.UpdateContacts do
+  use Ecto.Migration
+
+  def change do
+    alter table(:contacts) do
+      modify :mailerlite_id, :bigint
+    end
+  end
+end
