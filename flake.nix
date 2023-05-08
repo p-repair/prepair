398062ae2383caba8bc96951e5495cbe41d432f2
Jean-Philippe Cugnet <jean-philippe@cugnet.eu>
2023-05-08 16:46:25 +0200

    deps(nix): switch to a flake

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "The landing page for (p)repair.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOs/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, flake-utils, ... }@inputs:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import inputs.nixpkgs { inherit system; };
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            # Build toolchain.
+            elixir
+
+            # Project dependencies.
+            postgresql_15
+
+            # Development dependencies.
+            inotify-tools
+            libnotify
+
+            # Tools.
+            flyctl
+            git
+            gitAndTools.gitflow
+          ];
+
+          shellHook = ''
+            export PGDATA="$PWD/db"
+          '';
+        };
+      });
+}
