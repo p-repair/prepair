d087301ed97f658a6c87b3c3d767a284a0e6e7a9
Jean-Philippe Cugnet <jean-philippe@cugnet.eu>
2024-01-14 00:41:37 +0100

    env(devshell): switch to flake-parts + devshell
    Output of `nix store diff-closures`:
    
    -p: ∅ → ε
    audit-tmpdir.sh: ε → ∅
    compress-man-pages.sh: ε → ∅
    devshell: ∅ → ε, +188.7 KiB
    diffutils: 3.10 → ∅, -1573.4 KiB
    ed: 1.19 → ∅, -135.6 KiB
    file: -8534.1 KiB
    findutils: 4.9.0 → ∅, -1361.1 KiB
    gcc: +446.4 KiB
    gdk-pixbuf: -496.3 KiB
    gitsetup: ∅ → ε
    glib: -3518.0 KiB
    glibc-iconv: 2.38 → ∅
    gnu-config: 2023-09-19 → ∅, -85.6 KiB
    libelf: 0.8.13 → ∅, -335.4 KiB
    libffi: -20.8 KiB
    libjpeg-turbo: -434.4 KiB
    libnotify: -69.1 KiB
    libpng-apng: -255.1 KiB
    libtiff: -251.6 KiB
    make-symlinks-relative.sh: ε → ∅
    menu: ∅ → ε
    move-docs.sh: ε → ∅
    move-lib64.sh: ε → ∅
    move-sbin.sh: ε → ∅
    move-systemd-user-units.sh: ε → ∅
    multiple-outputs.sh: ε → ∅, -8.3 KiB
    nix: ε → ∅, -12.6 KiB
    patch: 2.7.6 → ∅, -222.5 KiB
    patch-shebangs.sh: ε → ∅
    patchelf: 0.15.0 → ∅, -235.0 KiB
    postgresql: +759.6 KiB
    prune-libtool-files.sh: ε → ∅
    reproducible-builds.sh: ε → ∅
    set-source-date-epoch-to-latest.sh: ε → ∅
    setup: ∅ → ε
    start: ∅ → ε
    stdenv: ε → ∅, -50.0 KiB
    stop: ∅ → ε
    strip.sh: ε → ∅
    update-autotools-gnu-config-scripts: ε → ∅
    
    Refs: #34

--- flake.nix
+++ flake.nix
@@ -3,36 +3,86 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
-    flake-utils.url = "github:numtide/flake-utils";
+
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+
+    devshell = {
+      url = "github:numtide/devshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, flake-utils, ... }@inputs:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import inputs.nixpkgs { inherit system; };
-      in
-      {
-        devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            # Build toolchain.
-            beam.packages.erlang_26.elixir_1_15
+  outputs = { flake-parts, ... }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [ inputs.devshell.flakeModule ];
+      systems = [ "x86_64-linux" ];
 
-            # Project dependencies.
-            postgresql_15
+      perSystem = { self', system, ... }:
+        let
+          pkgs = inputs.nixpkgs.legacyPackages.${system};
+        in
+        {
+          devshells.default = {
+            name = "(p)repair";
 
-            # Development dependencies.
-            inotify-tools
-            libnotify
+            motd = ''
 
-            # Tools.
-            flyctl
-            git
-            gitAndTools.gitflow
-          ];
+              {202}🔨 Welcome to the (p)repair devshell!{reset}
+              $(type -p menu &>/dev/null && menu)
+            '';
 
-          shellHook = ''
-            export PGDATA="$PWD/db"
-          '';
+            packages = with pkgs; [
+              # Build toolchain.
+              beam.packages.erlang_26.elixir_1_15
+              gcc
+              gnumake
+
+              # Project dependencies.
+              postgresql_15
+
+              # Development dependencies.
+              inotify-tools
+              libnotify
+
+              # Tools.
+              flyctl
+              git
+              gitAndTools.gitflow
+            ];
+
+            env = [
+              { name = "PGDATA"; eval = "$PWD/db"; }
+            ];
+
+            commands = [
+              {
+                name = "gitsetup";
+                help = "Configures git-flow in the local repo";
+                command = builtins.readFile ./scripts/gitsetup;
+              }
+
+              {
+                name = "setup";
+                help = "Compiles the application, and sets the database up";
+                command = builtins.readFile ./scripts/setup;
+              }
+
+              {
+                name = "start-db";
+                help = "Starts a local instance of PostgreSQL";
+                command = builtins.readFile ./scripts/start-db;
+              }
+
+              {
+                name = "stop-db";
+                help = "Stops the local instance of PostgreSQL";
+                command = builtins.readFile ./scripts/stop-db;
+              }
+            ];
+          };
         };
-      });
+    };
 }
